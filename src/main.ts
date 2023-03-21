fc65f693b96d026a993e67389ca249d9181ec3e2
hisham-mhammed-afifi <hisham.mohmmed@gmail.com>
2023-03-21 18:27:33 +0200

    sdfsdf

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,12 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    swaggerOptions: {
+      customJs:
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.0.0/swagger-ui-bundle.js',
+    },
+  });
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
